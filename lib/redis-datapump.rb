b82142b4da7a7ca1f496a99b7646ce203ec30ae2
Leif Gensert <leif@propertybase.com>
2013-08-02 00:15:13 +0200

    first working draft

--- lib/redis-datapump.rb
+++ lib/redis-datapump.rb
@@ -1,6 +1,7 @@
 require 'redis'
 require 'redis-datapump/version'
 require 'redis-datapump/exporter'
+require 'redis-datapump/importer'
 require 'redis-datapump/validator'
 require 'redis-datapump/value_extractor'
 
---
+++ lib/redis-datapump/importer.rb
@@ -0,0 +1,26 @@
+module RedisDatapump
+  class Importer
+    IMPORTER_MAP = {
+      'string' => proc {|client, key, value| client.set(key, value)},
+      'hash' => proc {|client, key, values| client.hmset(key, values)},
+      'set' => proc {|client, key, members| client.sadd(key, members)},
+      'list' => proc {|client, key| client.lrange key, 0, -1 },
+      'zset' => proc {|client, key| client.zrange(key, 0, -1, :with_scores => true) },
+    }
+    def initialize opts
+      @options = opts
+      Validator.new(@options).validate!
+    end
+
+    def import content
+      IMPORTER_MAP[content['type']].call(redis_client, content['key'], content['value'])
+      @redis_client.pexpire(content['key'], content['ttl']) if content['ttl'] != -1
+    end
+
+    def redis_client
+      return @redis_client if @redis_client
+      url = "#{@options[:redis_url]}/#{@options[:redis_database]}"
+      @redis_client = Redis.connect(url: url)
+    end
+  end
+end
